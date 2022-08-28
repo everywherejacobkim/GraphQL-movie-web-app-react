59d8a9b1959c406f999be0e61eccc506bf897d80
everywherejacobkim <everywherejacobkim@gmail.com>
2022-08-28 01:35:12 -0700

    Build: create router and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Movies from "./routes/Movies";
+import Movie from "./routes/Movie";
+
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Movies />} />
+        <Route path="/movies/:id" element={<Movie />} />
+      </Routes>
+    </BrowserRouter>
+      
   );
 }
 
