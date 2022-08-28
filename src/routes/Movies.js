479be92dd14fb5824be1df7b42ca5d3a00c42705
everywherejacobkim <everywherejacobkim@gmail.com>
2022-08-28 02:19:54 -0700

    Build: get gql data from server

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useApolloClient, gql } from '@apollo/client'
 
 const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const client = useApolloClient();
+  useEffect(() => {
+    client.query({
+      query: gql`
+        {
+          allMovies {
+            id
+            title
+          }
+        }
+      `,
+    }).then((results) => setMovies(results.data.allMovies))
+  }, [client]);
   return (
-    <div>This is a list of Movies</div>
+    <>
+      <div>This is a list of Movies</div>
+      <div>{movies.map(movie =>
+        <li key={movie.id}>{movie.title}</li>
+        )}</div>
+    </>
   )
 }
 
